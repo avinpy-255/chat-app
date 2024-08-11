b4edc9a2ddbca89c9cc8ca27986397b26a2462fb
Avinash <avinashchowdhury81@gmail.com>
2024-08-12 01:25:34 +0530

    added css and style

--- Frontend/src/components/list/chatlist/ChatList.jsx
+++ Frontend/src/components/list/chatlist/ChatList.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
+import './chatlist.css'
 
 const ChatList = () => {
+  const [addNode, setAddNode] = useState(false)
   return (
     <div className='chatList'>
     <div className='search'>
     <div className='searchBar'>
+    <img src='./srch.png' alt=""/>
       <input type='text' placeholder='Search'/>
-      <img src='./search.png' alt=""/>
     </div>
-     <img src='./plus.png' alt=""/>
+     <img src={addNode ? './minus.png' :'./plus.png'} alt="" className='add'
+       onClick={() => setAddNode((perv) => !perv)}
+     />
     </div>
-
+       <img src='./avatar.png' alt=""/>
+       <div className='texts'>
+           <span>sexy</span>
+           <p>Hello</p>
+       </div>
     </div>
   )
 }
